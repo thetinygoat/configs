348d16bbb63713617ee1844f8ec84297fb0e4893
Sachin Saini <iamsainisachin@gmail.com>
2021-04-30 23:31:03 +0530

    add readme and update rsync include and exclude paths

--- zsh/functions/rsync.sh
+++ zsh/functions/rsync.sh
@@ -1,9 +1,9 @@
 #rsync dry backup
 dback(){
-  rsync -av --dry-run $1 $2 --include-from="$HOME/Configs/.rsyncinclude" --exclude-from="$HOME/Configs/.rsyncignore"
+  rsync -av --dry-run $1 $2 --include-from="$HOME/Configs/rsync/include" --exclude-from="$HOME/Configs/rsync/ignore"
 }
 
 #rsync dry backup
 back(){
-  rsync -av $1 $2 --include-from="$HOME/Configs/.rsyncinclude" --exclude-from="$HOME/Configs/.rsyncignore"
+  rsync -av $1 $2 --include-from="$HOME/Configs/rsync/include" --exclude-from="$HOME/Configs/rsync/ignore"
 }
